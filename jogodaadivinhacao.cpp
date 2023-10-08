cf7ff06a3da2ce823bc3558140f7888460e702e3
Benefrancis <benefrancis@gmail.com>
2023-10-08 18:12:10 -0300

    utilizando o rand

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -1,5 +1,8 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
 
 int main(){
 
@@ -7,22 +10,40 @@ cout <<"**************************************"<< endl;
 cout <<"* Bem-vindos ao jogo da adivinhacao! *"<< endl;
 cout <<"**************************************"<< endl;
 
-const int NUMERO_SECRETO = 42;
+int numero_tentativas;
+
+srand(time(NULL));
+
+const int NUMERO_SECRETO = rand() % 100;
+
 int chute;
 bool acertou= false;
 int tentativas = 0;
-
 double pontos = 1000.0;
 
+cout << "Escolha o seu nivel de dificuldade: " << endl;
+cout << "Facil (F), Medio (M) ou Dificil(D)" << endl;
 
-while(!acertou){
+char dificuldade;
 
-tentativas ++;
-cout << "Tentativa "<< tentativas << endl;
-cout << "Qual eh o seu chute? ";
- cin >> chute;
+cin >> dificuldade;
 
- cout << "O valor do seu chute eh: " << chute << endl;
+if(dificuldade = 'F'){
+numero_tentativas = 15;
+}else if(dificuldade = 'M'){
+numero_tentativas = 10;
+
+}else{
+numero_tentativas = 5;
+}
+
+for(tentativas = 1; tentativas <= numero_tentativas ; tentativas ++) {
+
+    cout << "Tentativa "<< tentativas << endl;
+    cout << "Qual eh o seu chute? ";
+    cin >> chute;
+
+    cout << "O valor do seu chute eh: " << chute << endl;
 
     acertou = chute == NUMERO_SECRETO;
     bool maior = chute > NUMERO_SECRETO;
@@ -34,6 +55,7 @@ cout << "Qual eh o seu chute? ";
 
     if(acertou){
         cout << "Parabens! Voce acertou o numero secreto!" << endl;
+        break; //Interrompendo o laço de repetição
     }else if(maior){
         cout << "Seu chute foi maior do que o numero secreto!" << endl;
     }else{
@@ -42,8 +64,14 @@ cout << "Qual eh o seu chute? ";
 
 }
 cout << "Fim de jogo...";
+
+if(acertou){
 cout << "Voce acertou o numero secreto na " << tentativas << "a tentativa! " << endl;
 cout.precision(2);
 cout << fixed;
 cout << "Sua pontuacao foi de: " << pontos << endl;
+}else{
+cout << "Voce perdeu! Tente novamente!" << endl;
+}
+
 }
