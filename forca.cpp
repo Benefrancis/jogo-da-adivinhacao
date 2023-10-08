2a329697339bc1c075a1bcb9e1565c77ea0e40d3
Benefrancis <benefrancis@gmail.com>
2023-10-08 19:19:23 -0300

    Compilando na versão 14 C++
    utilizando o map<char, bool>

--- forca.cpp
+++ forca.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 #include<string>
+#include <map>
+
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
 
-bool existe(char chute){
+map<char, bool> chutou;
 
-    for(char c: PALAVRA_SECRETA){
-        if(chute == c){
+
+bool existe(char chute) {
+
+    for (char c: PALAVRA_SECRETA) {
+        if (chute == c) {
             return true;
         }
     }
@@ -15,17 +20,30 @@ bool existe(char chute){
 }
 
 
-int main(){
-    cout << PALAVRA_SECRETA << endl;
+int main() {
+   // cout << PALAVRA_SECRETA << endl;
     bool acertou;
     bool enforcou;
-    while(!enforcou || !acertou){
+
+    while (!enforcou || !acertou) {
+
+        for (char l: PALAVRA_SECRETA) {
+            if (chutou[l]) {
+                cout << l << " ";
+            } else {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
         char chute;
-        cout << "Chute uma letra: "<< endl;
+        cout << "Chute uma letra: " << endl;
         cin >> chute;
-        if(existe(chute)){
+
+        chutou[chute] = true;
+
+        if (existe(chute)) {
             cout << "O seu chute esta na palavra" << endl;
-        }else{
+        } else {
             cout << "O seu chute nao esta na palavra" << endl;
         }
     }
